2b2a77bd3e078067e6b285abf6414443a9c18124
zyc <cen66673@163.com>
2022-06-24 16:41:10 +0800

    用户数据压力测试

--- seckill/src/main/java/com/zyc/seckill/controller/UserController.java
+++ seckill/src/main/java/com/zyc/seckill/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.zyc.seckill.controller;
 
 
+import com.zyc.seckill.pojo.User;
+import com.zyc.seckill.vo.RespBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -15,5 +18,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    /**
+    * @description: 用户信息（测试使用）
+    * @param:
+    * @return:
+    * @author zyc
+    * @date: 2022/6/24 14:17
+    */
+    @RequestMapping("/info")
+    @ResponseBody
+    public RespBean info(User user){
+        return RespBean.success(user);
+    }
 
 }
